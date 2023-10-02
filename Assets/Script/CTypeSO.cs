50a570601a35adafbcb3cfefee104363105080d0
VsPot07 <cocoacake84@gmail.com>
2023-10-02 21:40:30 +0900

    1002

---
+++ Assets/Script/CTypeSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/CharacterSO")]
+public class CTypeSO : ScriptableObject
+{
+    public Texture2D[] Characters;
+}
---
+++ Assets/Script/Define.cs
@@ -0,0 +1,17 @@
+public enum Emotion
+{
+
+}
+
+public enum Clothes
+{
+    Casual = 0,
+    Gym,
+    Maid,
+    Summer
+}
+
+public enum BackGround
+{
+
+}
